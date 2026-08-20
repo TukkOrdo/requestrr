f9e1c6cfdd4e059e28a2f552b84a4f7bbb9d28f8
Jordan Cooper <tukkordo@gmail.com>
2026-08-19 19:57:27 -0600

    Updated version number to 2.1.11

--- Requestrr.WebApi/RequestrrBot/Locale/Language.cs
+++ Requestrr.WebApi/RequestrrBot/Locale/Language.cs
@@ -34,7 +34,7 @@ namespace Requestrr.WebApi.RequestrrBot.Locale
 #if DEBUG
         public static string BuildVersion = "Development";
 #else
-        public static string BuildVersion = "v2.1.10";
+        public static string BuildVersion = "v2.1.11";
 #endif
 
         public string Error { get; set; }
